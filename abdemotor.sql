7cf8833b74feda5c44a3c480ec59179deb6a5aa8
hachimabdel <abdelhachim@icloud.com>
2024-10-22 14:04:49 +0200

    Se me olvido la base de datos

--- abdemotor.sql
+++ abdemotor.sql
@@ -8,38 +8,39 @@ CREATE TABLE coche (
     marca VARCHAR(50),
     color VARCHAR(50)
 );
-/*Select *
+/*USE abdemotor;
+Select *
 fROM coche;
 
 DELETE FROM coche
-WHERE marca = 'Lamborghini';*/
+WHERE marca = 'Lamborghini';
+
+DELETE FROM coche;*/
+
 
 
 INSERT INTO coche (tipoVehiculo, marca, color) VALUES 
-    ('SUV', 'Mercedes GLC', 'Gris metalizado'),
-    ('SUV', 'Mercedes GLE', 'Blanco'),
-    ('SUV', 'Mercedes GLS', 'Negro'),
-    ('SUV', 'Mercedes EQB', 'Rojo'),
-    ('SUV', 'Audi Q7', 'Gris metalizado'),
-    ('SUV', 'Audi Q5', 'Blanco'),
-    ('SUV', 'Audi Q3', 'Negro'),
-    ('SUV', 'Audi Q8', 'Plata'),
-    ('SUV', 'Lamborghini Urus', 'Gris metalizado'),
-    ('SUV', 'Lamborghini LM002', 'Blanco'),
-    ('SUV', 'Ferrari Purosangue', 'Rojo'),
-    ('Sedán', 'Mercedes C63', 'Gris metalizado'),
-    ('Sedán', 'Mercedes E-Class', 'Blanco'),
-    ('Sedán', 'Mercedes S-Class', 'Negro'),
-    ('Sedán', 'Audi A4', 'Rojo'),
-    ('Sedán', 'Audi A6', 'Plata'),
-    ('Sedán', 'Audi A8', 'Gris metalizado'),
-    ('Sedán', 'Lamborghini Estoque', 'Rojo'),
-    ('Sedán', 'Ferrari GTC4Lusso', 'Blanco'),
-    ('Deportivo', 'Lamborghini Aventador', 'Negro'),
-    ('Deportivo', 'Lamborghini Huracán', 'Rojo'),
-    ('Deportivo', 'Ferrari 488 GTB', 'Blanco'),
-    ('Deportivo', 'Ferrari F8 Tributo', 'Gris metalizado');
-
-
-   
-
+    ('SUV', 'Mercedes', 'Gris metalizado'),
+    ('SUV', 'Mercedes', 'Blanco'),
+    ('SUV', 'Mercedes', 'Negro'),
+    ('SUV', 'Mercedes', 'Rojo'),
+    ('SUV', 'Audi', 'Gris metalizado'),
+    ('SUV', 'Audi', 'Blanco'),
+    ('SUV', 'Audi', 'Negro'),
+    ('SUV', 'Audi', 'Plata'),
+    ('SUV', 'Lamborghini', 'Gris metalizado'),
+    ('SUV', 'Lamborghini', 'Blanco'),
+    ('SUV', 'Ferrari', 'Rojo'),
+    ('Sedán', 'Mercedes', 'Gris metalizado'),
+    ('Sedán', 'Mercedes', 'Blanco'),
+    ('Sedán', 'Mercedes', 'Negro'),
+    ('Sedán', 'Audi', 'Rojo'),
+    ('Sedán', 'Audi', 'Plata'),
+    ('Sedán', 'Audi', 'Gris metalizado'),
+    ('Sedán', 'Lamborghini', 'Rojo'),
+    ('Sedán', 'Ferrari', 'Blanco'),
+    ('Deportivo', 'Lamborghini', 'Negro'),
+    ('Deportivo', 'Lamborghini', 'Rojo'),
+    ('Deportivo', 'Ferrari', 'Blanco'),
+    ('Deportivo', 'Ferrari', 'Gris metalizado');
+    
